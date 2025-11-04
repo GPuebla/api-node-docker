df2745ebdaf2ead4b638cce0bca49c41229abebc
gpuebla <gabrielpuebla16@gmail.com>
2025-11-04 15:53:48 -0300

    creating new docker files

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hola desde Docker!' });
+  res.json({message:'Hola desde Docker!'});
+});
+
+app.get('/1', (req, res) => {
+  res.json({message:'Respuesta 123'});
+});
+
+app.get('/2', (req, res) => {
+  res.send('Funcionaaaaaa!!!');
 });
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
