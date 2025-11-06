e81ccea36f8a777ab721a0816c9189c11ec41f09
gpuebla <gabrielpuebla16@gmail.com>
2025-11-06 14:43:34 -0300

    updating route file

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import userController from '../controllers/userController.js';
+import userController from '../controllers/userController';
 
 // Crear el router
 const router = express.Router();
 
 // Definir rutas
 router.get('/', userController.getUsers);
+router.get('/1', (req,res)=>{res.send("Test")});
 router.get('/2', userController.getUsers2);
 router.post('/', userController.createUser);
 
