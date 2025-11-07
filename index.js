ad01eae756ee4f203583a303e33475b36f2d371a
gpuebla <gabrielpuebla16@gmail.com>
2025-11-07 15:17:34 -0300

    updating docker config

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 //Routes
+app.use('/', (req,res)=>{res.send('Welcome Perris')});
 app.use('/api/users',userRoutes);
+
+console.log("Servidor actualizado");
+
 
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
