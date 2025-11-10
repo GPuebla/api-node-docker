16bef1debc196c88cbca7421567a7db2289e213d
gpuebla <gabrielpuebla16@gmail.com>
2025-11-10 15:16:14 -0300

    last update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/api/users',userRoutes);
 app.use('/ac',accountRoute);
-app.use('/', (req,res)=>{res.send('Welcome')});
+app.use('/', (req,res)=>{res.send('Welcome 1')});
 
 
 
