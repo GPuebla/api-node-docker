ae84294433f32efea4d2b2f23f98a5f3e5e2932a
gpuebla <gabrielpuebla16@gmail.com>
2025-11-11 15:47:06 -0300

    Creating errorHandler global

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,24 @@
+// src/middleware/errorHandler.js
+export const errorHandler = (err, req, res, next) => {
+    console.error("🔥 Error detectado:", err.message);
+  
+    const status = err.status || 500;
+  
+    if (err.name === "ValidationError") {
+      return res.status(400).json({
+        message: "Error de validación",
+        details: err.errors,
+      });
+    }
+  
+    if (err.name === "CastError") {
+      return res.status(400).json({
+        message: "ID inválido o formato incorrecto",
+      });
+    }
+  
+    res.status(status).json({
+      message: err.message || "Error interno del servidor",
+    });
+  };
+  
