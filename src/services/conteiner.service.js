64a26767747f240778a6440846aaa741df446080
gpuebla <gabrielpuebla16@gmail.com>
2025-11-27 17:33:12 -0300

    updating container.service (adding import - models)

--- src/services/conteiner.service.js
+++ src/services/conteiner.service.js
@@ -1,56 +1,59 @@
-import Container from "../models/container.model.js";
+  import Container from "../models/container.model.js";
+  import Transport from "../models/transport.model.js";
+  import Booking from "../models/booking.model.js";
 
 
-class ContainerService {
 
-  // Crear un contenedor
-  async create(data) {
-    return await Container.create(data);
-  }
+  class ContainerService {
 
-  // Obtener todos los contenedores
-  async getAll() {
-    return await Container.find().populate("booking transport");
-  }
-
-  // Obtener uno por ID
-  async getById(id) {
-    const container = await Container.findById(id).populate("booking transport");
-    if (!container) throw new Error("Container not found");
-    return container;
-  }
-
-  // Actualizar contenedor
-  async update(id, data) {
-    const updated = await Container.findByIdAndUpdate(id, data, { new: true });
-    if (!updated) throw new Error("Container not found");
-    return updated;
-  }
-
-  // Contenedores por booking
-  async getByBooking(bookingId) {
-    return await Container.find({ booking: bookingId });
-  }
-
-  // Reasignar settings reefer
-  async reassignSettings(containerId, reservationSettings) {
-    const container = await this.getById(containerId);
-
-    if (!container.isReefer) {
-      throw new Error("Container is not reefer, cannot assign settings");
+    // Crear un contenedor
+    async create(data) {
+      return await Container.create(data);
     }
 
-    container.reeferSettings = reservationSettings;
+    // Obtener todos los contenedores
+    async getAll() {
+      return await Container.find().populate("booking transport");
+    }
 
-    return await container.save();
+    // Obtener uno por ID
+    async getById(id) {
+      const container = await Container.findById(id).populate("booking transport");
+      if (!container) throw new Error("Container not found");
+      return container;
+    }
+
+    // Actualizar contenedor
+    async update(id, data) {
+      const updated = await Container.findByIdAndUpdate(id, data, { new: true });
+      if (!updated) throw new Error("Container not found");
+      return updated;
+    }
+
+    // Contenedores por booking
+    async getByBooking(bookingId) {
+      return await Container.find({ booking: bookingId });
+    }
+
+    // Reasignar settings reefer
+    async reassignSettings(containerId, reservationSettings) {
+      const container = await this.getById(containerId);
+
+      if (!container.isReefer) {
+        throw new Error("Container is not reefer, cannot assign settings");
+      }
+
+      container.reeferSettings = reservationSettings;
+
+      return await container.save();
+    }
+
+    // Cambiar estado del contenedor
+    async updateStatus(id, status) {
+      const cont = await this.getById(id);
+      cont.status = status;
+      return await cont.save();
+    }
   }
 
-  // Cambiar estado del contenedor
-  async updateStatus(id, status) {
-    const cont = await this.getById(id);
-    cont.status = status;
-    return await cont.save();
-  }
-}
-
-export default new ContainerService();
+  export default new ContainerService();
