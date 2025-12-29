a430e6abde26dd5bea9835695fc5a73717dd43fc
Gabriel Puebla <gabrielpuebla16@gmail.com>
2025-12-29 08:37:54 -0300

    updating port controller

--- src/controllers/port.controller.js
+++ src/controllers/port.controller.js
@@ -1,3 +1,6 @@
 import BaseController from './base.controller.js';
 import PortService from '../services/port.service.js';
 
+const portController = BaseController.createBaseController(PortService);
+
+export default portController;
