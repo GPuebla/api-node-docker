e18bfa7dbca49710ad3378b435ef5b9d5366b69f
gpuebla <gabrielpuebla16@gmail.com>
2025-12-29 11:39:48 -0300

    improving base Service

--- src/services/base.service.js
+++ src/services/base.service.js
@@ -1,14 +1,50 @@
 
+// class BaseService {
+
+//     createBaseService = (Model) => ({
+//         create: (data) => Model.create(data),
+//         getAll: () => Model.find(),
+//         getById: (id) => Model.findById(id),
+//         update: (id, data) => Model.findByIdAndUpdate(id, data, { new: true }),
+//         remove: (id) => Model.findByIdAndDelete(id),
+//     });
+
+// }
+
+// export default new BaseService;
+
 class BaseService {
-
-    createBaseService = (Model) => ({
+    createBaseService(Model, options = {}) {
+      const { populate = "" } = options;
+  
+      return {
         create: (data) => Model.create(data),
-        getAll: () => Model.find(),
-        getById: (id) => Model.findById(id),
-        update: (id, data) => Model.findByIdAndUpdate(id, data, { new: true }),
-        remove: (id) => Model.findByIdAndDelete(id),
-    });
+  
+        getAll: (filter = {}) =>
+          Model.find(filter).populate(populate),
+  
+        async getById(id) {
+          const doc = await Model.findById(id).populate(populate);
+          if (!doc) throw new Error(`${Model.modelName} not found`);
+          return doc;
+        },
+  
+        update: (id, data) =>
+          Model.findByIdAndUpdate(id, data, { new: true, runValidators: true }),
+  
+        remove: (id) =>
+          Model.findByIdAndDelete(id),
+      };
+    }
+  }
+  
+  export default new BaseService();
 
-}
 
-export default new BaseService;
+  //EJEMPLO DE USO
+
+//   const containerService = BaseService.createBaseService(Container, {
+//     populate: "booking transport",
+//   });
+  
+
