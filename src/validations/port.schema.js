92921a28b8117ef609525836dff35dc82b4b9efc
Gabriel Puebla <gabrielpuebla16@gmail.com>
2026-01-16 08:23:44 -0300

    creating portValidation

---
+++ src/validations/port.schema.js
@@ -0,0 +1,13 @@
+import Joi, {required, string} from "joi";
+
+const createPortSchema = Joi.object({
+    name:Joi.string().required(),
+    country:Joi.string(),
+})
+
+const updatePortSchema = Joi.object({
+    name:Joi.string(),
+    country:Joi.string(),
+})
+
+export default {createPortSchema, updatePortSchema};
