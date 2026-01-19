d25a64e468c6be27f1f16ebf8c170d7e62554b1b
gpuebla <gabrielpuebla16@gmail.com>
2026-01-19 16:31:38 -0300

    creating usaer and auth middleware

---
+++ src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model.User || model("User", userSchema);
