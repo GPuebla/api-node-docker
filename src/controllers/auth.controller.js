65cb3e60ca6025d6246d254dd0ad5d0b280d07bc
Gabriel Puebla <gabrielpuebla16@gmail.com>
2026-01-27 23:57:35 -0300

    refactor: remove commented-out code in AuthController

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,29 +1,3 @@
-// import AuthService from "../services/auth.service.js";
-
-// class AuthController {
-//   async login(req, res) {
-//     const { email, password } = req.body;
-
-//     if (!email || !password) {
-//       return res.status(400).json({
-//         message: "Email and password are required",
-//       });
-//     }
-
-//     const token = await AuthService.login(email, password);
-
-//     if (!token) {
-//       return res.status(401).json({
-//         message: "Invalid credentials",
-//       });
-//     }
-
-//     return res.json({ token });
-//   }
-// }
-
-// export default new AuthController();
-
 import AuthService from "../services/auth.service.js";
 
 class AuthController {
