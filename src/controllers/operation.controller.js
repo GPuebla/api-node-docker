c39276319f9cccf5ea081c733c6bbdb0d92b4330
GPuebla <gabrielpuebla16@gmail.com>
2026-01-31 20:34:20 -0300

    refactor: uncomment operation controller logic and imports

--- src/controllers/operation.controller.js
+++ src/controllers/operation.controller.js
@@ -1,43 +1,43 @@
-// import BaseController from "./base.controller.js";
-// import operationService from "../services/operation.service.js";
-// import Counter from "../models/counter.model.js";
+import BaseController from "./base.controller.js";
+import operationService from "../services/operation.service.js";
+import Counter from "../models/counter.model.js";
 
-// const operationController = BaseController.createBaseController(operationService);
+const operationController = BaseController.createBaseController(operationService);
 
-//PUT /operations/:id/confirm
+// PUT /operations/:id/confirm
 
 
-// const confirmOperation = async (req, res, next) => {
-//   try {
-//     const operation = await Operation.findById(req.params.id);
+operationController.confirmOperation = async (req, res, next) => {
+  try {
+    const operation = await Operation.findById(req.params.id);
 
-//     if (!operation) {
-//       return res.status(404).json({ message: "Operation not found" });
-//     }
+    if (!operation) {
+      return res.status(404).json({ message: "Operation not found" });
+    }
 
-//     // Business rule: only pending operations can be confirmed
-//     if (operation.status !== "pending") {
-//       return res.status(400).json({
-//         message: "Only pending operations can be confirmed",
-//       });
-//     }
+    // Business rule: only pending operations can be confirmed
+    if (operation.status !== "pending") {
+      return res.status(400).json({
+        message: "Only pending operations can be confirmed",
+      });
+    }
 
-//     // Get next operation number
-//     const counter = await Counter.findOneAndUpdate(
-//       { name: "operation" },
-//       { $inc: { seq: 1 } },
-//       { new: true, upsert: true }
-//     );
+    // Get next operation number
+    const counter = await Counter.findOneAndUpdate(
+      { name: "operation" },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
 
-//     operation.opNumber = counter.seq;
-//     operation.status = "confirmed";
+    operation.opNumber = counter.seq;
+    operation.status = "confirmed";
 
-//     await operation.save();
+    await operation.save();
 
-//     res.json(operation);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+    res.json(operation);
+  } catch (error) {
+    next(error);
+  }
+};
 
-// export default {operationController};
+export default {operationController};
