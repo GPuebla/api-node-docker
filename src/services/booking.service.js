5bf904eea202601fe4fa062c707e7b1e060bb60a
gpuebla <gabrielpuebla16@gmail.com>
2026-02-02 16:47:39 -0300

    refactor: update container schema and service to manage booking associations

--- src/services/booking.service.js
+++ src/services/booking.service.js
@@ -1,10 +1,10 @@
 import BaseService from "./base.service.js"
 import Booking from "../models/booking.model.js"
-import Port from "../models/port.model.js";
-import Vessel from "../models/vessel.model.js";
-import Line from "../models/line.model.js";
-import Shipper from "../models/shipper.model.js";
-import Consignee from "../models/consignee.model.js";
+// import Port from "../models/port.model.js";
+// import Vessel from "../models/vessel.model.js";
+// import Line from "../models/line.model.js";
+// import Shipper from "../models/shipper.model.js";
+// import Consignee from "../models/consignee.model.js";
 
 // Create booking service using BaseService (generic CRUD)
 const bookingService = BaseService.createBaseService(Booking, {
